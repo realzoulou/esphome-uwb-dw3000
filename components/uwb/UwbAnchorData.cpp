realzoulou/esphome-uwb-dw3000

commit cb0ce6850c9a2740828a6a70e9228f0f71517afb

Configurable anchor_away_after_ms, default 30000ms, min 1000ms

components/uwb/UwbAnchorData.cpp
@@ -9,13 +9,37 @@ const char* UwbAnchorData::TAG = "tag"; // running in role=tag context
 
 void UwbAnchorData::setDistance(double distanceMeters) {
     const uint32_t now = millis();
-    /* diff in [m] to previous distance. */
-    const double diffToPrevDistance = distanceMeters - mDistanceToTag;
-    /* diff in [ms] to previous millis() when distance was calculated. */
-    const double diffMillis = (double) (now - mMillisDistanceToTag);
-    /* speed in [m/s] that the tag seems to have changed its distance. */
-    const double speed = distanceMeters * 1000.0 / diffMillis;
 
+    if (std::isnan(distanceMeters)) {
+        if (mSensor != nullptr) {
+            auto sensor = const_cast<sensor::Sensor*>(mSensor); // remove 'const' in order to publish_state
+            // don't update mMillisDistanceToTag to now
+            mDistanceToTag = NAN;
+            sensor->publish_state(NAN);
+        }
+        ESP_LOGW(TAG, "anchor 0x%02X away", mId);
+        return;
+    }
+
+    double diffToPrevDistance, diffMillis, speed;
+    if (! std::isnan(mDistanceToTag)) {
+        /* diff in [m] to previous distance. */
+        diffToPrevDistance = distanceMeters - mDistanceToTag;
+        /* diff in [ms] to previous millis() when distance was calculated. */
+        diffMillis = (double) (now - mMillisDistanceToTag);
+        /* speed in [m/s] that the tag seems to have changed its distance. */
+        speed = distanceMeters * 1000.0 / diffMillis;
+    } else {
+        // set values such that outlier detection is passed.
+        diffToPrevDistance = MIN_DISTANCE;
+        diffMillis = NAN;
+        speed = MAX_SPEED;
+        if (!std::isnan(distanceMeters)) {
+            // changed from 'away' to 'back'
+            ESP_LOGW(TAG, "anchor 0x%02X back", mId);
+        }
+    }
+    
     /* Avoid outliers. */
     if (std::fabs(diffToPrevDistance) >= MIN_DISTANCE) {
         if (std::fabs(speed) <= MAX_SPEED) {

components/uwb/UwbAnchorData.h
@@ -51,7 +51,7 @@ private:
     const double MAX_SPEED;
 
     /* Latest Distance in [m] to tag. */
-    double mDistanceToTag{0.0};
+    double mDistanceToTag{NAN};
 
     /* Latest millis() when distance was set. */
     uint32_t mMillisDistanceToTag{0};

components/uwb/UwbComponent.cpp
@@ -27,7 +27,7 @@ void UwbComponent::setup() {
                     }
                 }
             }
-            mDevice = new UwbTagDevice(mAnchors, mRangingIntervalMs,
+            mDevice = new UwbTagDevice(mAnchors, mRangingIntervalMs, mMaxAgeAnchorDistanceMs,
                                        mLatitudeSensor, mLongitudeSensor, mLocationErrorEstimateSensor);
              // Anchors and Sensors no longer needed
             mAnchors.clear();

components/uwb/UwbComponent.h
@@ -32,6 +32,9 @@ const static double MAX_SPEED_DEFAULT           = 1.0;  /* 1 m/s */
 /* Default ranging interval time in [ms]. */
 const static uint32_t RANGING_INTERVAL_TIME_DEFAULT = 5000; /* every 5 s */
 
+/* Default maximum age of tag->anchor distance measurement until anchor is considered 'away'. */
+const static uint32_t MAX_AGE_ANCHOR_DISTANCE_DEFAULT = 30000; /* 30 s */
+
 class UwbComponent : public esphome::Component {
 
 public:
@@ -47,6 +50,7 @@ public:
     inline void setDeviceId(const uint8_t id) { mDeviceId = id; }
     inline void setRole(const eUwbRole role) { mRole = role; };
     inline void setRangingInterval(const uint32_t rangingIntervalMs) { mRangingIntervalMs = rangingIntervalMs; }
+    inline void setMaxAgeAnchorDistance(const uint32_t maxAgeAnchorDistanceMs) { mMaxAgeAnchorDistanceMs = maxAgeAnchorDistanceMs; }
     void addAnchor(
         // mandatory parameters
         const uint8_t id, const double latitude, const double longitude,
@@ -66,6 +70,7 @@ private:
     eUwbRole mRole{UWB_ROLE_UNKNOWN};
     uint8_t mDeviceId{0};
     uint32_t mRangingIntervalMs{RANGING_INTERVAL_TIME_DEFAULT};
+    uint32_t mMaxAgeAnchorDistanceMs{MAX_AGE_ANCHOR_DISTANCE_DEFAULT};
     std::vector<std::shared_ptr<UwbAnchorData>> mAnchors;
     std::map<const uint8_t, const sensor::Sensor*> mDistanceSensors;
     esphome::sensor::Sensor * mLatitudeSensor{nullptr};

components/uwb/UwbTagDevice.cpp
@@ -25,11 +25,13 @@ const char* UwbTagDevice::STATE_TAG = "tag_STATE";
 
 UwbTagDevice::UwbTagDevice(const std::vector<std::shared_ptr<UwbAnchorData>> & anchors,
                            const uint32_t rangingIntervalMs,
+                           const uint32_t maxAgeAnchorDistanceMs,
                            sensor::Sensor* latitudeSensor,
                            sensor::Sensor* longitudeSensor,
                            sensor::Sensor* locationErrorEstimateSensor)
 : RX_BUF_LEN(std::max(ResponseMsg::FRAME_SIZE, FinalMsg::FRAME_SIZE)),
-  RANGING_INTERVAL_MS(rangingIntervalMs)
+  RANGING_INTERVAL_MS(rangingIntervalMs),
+  MAX_AGE_ANCHOR_DISTANCE_MS(maxAgeAnchorDistanceMs)
 {
     mAnchors = std::move(anchors);
     mAnchorCurrentRangingSuccess.reserve(mAnchors.size());
@@ -538,6 +540,15 @@ void UwbTagDevice::rangingDone(bool success) {
     } else {
         const uint8_t attempts = mAnchorCurrentRangingSuccess[mCurrentAnchorIndex] -1;
         mAnchorCurrentRangingSuccess[mCurrentAnchorIndex] = attempts;
+        if (attempts == 0) {
+            const uint32_t now = millis();
+            uint32_t lastDistanceMs;
+            (mAnchors[mCurrentAnchorIndex])->getDistance(&lastDistanceMs);
+            if (now - lastDistanceMs > MAX_AGE_ANCHOR_DISTANCE_MS) {
+                // set anchor as 'away'
+                (mAnchors[mCurrentAnchorIndex])->setDistance(NAN);
+            }
+        }
     }
 
     // next anchor
@@ -546,6 +557,7 @@ void UwbTagDevice::rangingDone(bool success) {
 
 void UwbTagDevice::calculateLocation() {
     if (mAnchors.size() > 1) {
+        const uint32_t now = millis();
         // collect all distances to anchors
         std::vector<AnchorPositionTagDistance> anchorPositionAndTagDistances;
         for(const auto anchor: mAnchors) {
@@ -553,8 +565,9 @@ void UwbTagDevice::calculateLocation() {
             anchorPosAndTagDist.anchorId = anchor->getId();
             anchorPosAndTagDist.anchorPosition.latitude =  anchor->getLatitude();
             anchorPosAndTagDist.anchorPosition.longitude = anchor->getLongitude();
-            anchorPosAndTagDist.tagDistance = anchor->getDistance(nullptr); // no need for the age of the distance
-            if (Location::isValid(anchorPosAndTagDist)) {
+            uint32_t millisDistance;
+            anchorPosAndTagDist.tagDistance = anchor->getDistance(&millisDistance);
+            if (Location::isValid(anchorPosAndTagDist) && ((now - millisDistance) <= MAX_AGE_ANCHOR_DISTANCE_MS)) {
                 anchorPositionAndTagDistances.push_back(anchorPosAndTagDist);
             }
         }
@@ -583,7 +596,7 @@ void UwbTagDevice::calculateLocation() {
                     tagPosition.latitude, tagPosition.longitude, errorEstimateMeters);
             }
         } else {
-            ESP_LOGW(TAG, "calculatePosition failed: %i", res);
+            ESP_LOGW(TAG, "calculate position failed: 0x%02X", res);
         }
     }
     // next ranging cycle

components/uwb/UwbTagDevice.h
@@ -71,7 +71,8 @@ class UwbTagDevice : public Dw3000Device {
     static const uint32_t WAIT_RX_TIMEOUT_MS = 100;
 
 public:
-    UwbTagDevice(const std::vector<std::shared_ptr<UwbAnchorData>> & anchors, const uint32_t rangingIntervalMs,
+    UwbTagDevice(const std::vector<std::shared_ptr<UwbAnchorData>> & anchors,
+                 const uint32_t rangingIntervalMs, const uint32_t maxAgeAnchorDistanceMs,
                  sensor::Sensor* latitudeSensor, sensor::Sensor* longitudeSensor, sensor::Sensor* locationErrorEstimateSensor);
 
     ~UwbTagDevice();
@@ -107,6 +108,8 @@ protected:
 
     /* Time in millis between two ranging Initial messages. */
     const uint32_t RANGING_INTERVAL_MS;
+    /* Maximum age of distance to anchor until anchor is considered 'away'. */
+    const uint32_t MAX_AGE_ANCHOR_DISTANCE_MS;
 
     /* Array of all Anchors that this tag shall do ranging with. */
     std::vector<std::shared_ptr<UwbAnchorData>> mAnchors;

components/uwb/__init__.py
@@ -23,6 +23,7 @@ CONF_UWB_ROLE_ANCHOR = "anchor"
 CONF_UWB_ROLE_TAG = "tag"
 CONF_TAG_ANCHORS = "anchors"
 CONF_TAG_RANGING_INTERVAL = "ranging_interval_ms"
+CONF_TAG_ANCHOR_AWAY_DURATION = "anchor_away_after_ms"
 
 eUwbRole = uwb_ns.enum("eUwbRole")
 UWB_ROLE = {
@@ -95,6 +96,7 @@ CONFIG_SCHEMA = cv.Schema(
             cv.Length(min=1, msg=f"{CONF_UWB_ROLE} '{CONF_UWB_ROLE_TAG}' must define at least 1 {CONF_TAG_ANCHORS}")
         ),
         cv.Optional(CONF_TAG_RANGING_INTERVAL): cv.int_range(min=1000),
+        cv.Optional(CONF_TAG_ANCHOR_AWAY_DURATION): cv.int_range(min=1000),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 cv.only_with_arduino
@@ -114,6 +116,8 @@ async def to_code(config):
             cg.add(var.addAnchor(anchor[CONF_UWB_DEVICE_ID], anchor[CONF_LATITUDE], anchor[CONF_LONGITUDE]))
             if ranging_interval := config[CONF_TAG_RANGING_INTERVAL]:
                 cg.add(var.setRangingInterval(ranging_interval))
+            if anchor_away_duration := config[CONF_TAG_ANCHOR_AWAY_DURATION]:
+                cg.add(var.setMaxAgeAnchorDistance(anchor_away_duration))
 
     # ----- General compiler settings
     # treat warnings as error, abort compilation on the first error, check printf format and arguments

commit e1999546d44a5a3fe719ee33953b127949e29449

Fix regression introduced with [cb0ce68] Configurable anchor_away_after_ms

components/uwb/UwbAnchorData.cpp
@@ -54,6 +54,8 @@ void UwbAnchorData::setDistance(double distanceMeters) {
                 mId, distanceMeters, diffToPrevDistance, diffMillis, speed, MAX_SPEED, mDistanceToTag);
         }
     } else {
+        // update timestamp nevertheless to avoid 'away' detection kicks in
+        mMillisDistanceToTag = now;
         // not a warning because this happens very frequently and is actually normal
         ESP_LOGI(TAG, "anchor 0x%02X: dist %.2fm (prev %.2fm) < threshold %.2fm, keep %.2fm",
             mId, distanceMeters, diffToPrevDistance, MIN_DISTANCE, mDistanceToTag);